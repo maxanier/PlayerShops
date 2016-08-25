2f87675a485eef9869afcb229bfec8c5a8685731
Nentify <nentify@live.co.uk>
2016-08-25 19:33:09 +0100

    Utils should be lowercase (utils)

--- src/main/java/me/nentify/playershops/events/BlockEventHandler.java
+++ src/main/java/me/nentify/playershops/events/BlockEventHandler.java
@@ -2,7 +2,7 @@ package me.nentify.playershops.events;
 
 import me.nentify.playershops.PlayerShops;
 import me.nentify.playershops.ShopType;
-import me.nentify.playershops.Utils.ChestUtils;
+import me.nentify.playershops.utils.ChestUtils;
 import me.nentify.playershops.data.PlayerShopData;
 import net.minecraft.inventory.IInventory;
 import org.spongepowered.api.Sponge;
